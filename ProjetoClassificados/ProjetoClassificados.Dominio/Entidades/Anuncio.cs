2fb84cbbbece88640f17be5306bdc0001ca45912
Rafael-P <r4f4el_porto@hotmail.com>
2021-09-13 12:32:19 -0300

    dominios e comands adicionados

---
+++ ProjetoClassificados/ProjetoClassificados.Dominio/Entidades/Anuncio.cs
@@ -0,0 +1,64 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using ProjetoClassificados.Comum.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoClassificados.Dominio.Entidades
+{
+    public class Anuncio : Entidade
+    {
+
+        public Anuncio(string nome, string descricao, string imagem, float preco)
+        {
+            AddNotifications(
+            new Contract<Notification>()
+                .Requires()
+                .IsNotEmpty(nome, "Nome", "Nome não pode ser vazio")
+                .IsNotEmpty(descricao, "Descricao", "Descricao não pode ser vazia")
+            );
+
+            if (IsValid)
+            {
+                Nome = nome;
+                Descricao = descricao;
+                Imagem = imagem;
+                Preco = preco;
+            }
+
+        }
+
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public string Imagem { get; private set; }
+        public float Preco { get; private set; }
+
+        // Composicões ? id do usuario q postou o anuncio
+
+        // public Guid idUsuario { get; private set; }
+        // public virtual Usuario Usuario { get; private set; }
+
+
+        public void AtualizaAnuncio(string descricao, string imagem, float preco)
+        {
+            AddNotifications(
+            new Contract<Notification>()
+                .Requires()
+                .IsNotEmpty(descricao, "Descrição", "Descrição não pode ser vazia")
+            );
+
+            if (IsValid)
+            {
+                Descricao = descricao;
+                Imagem = imagem;
+                Preco = preco;
+            }
+
+        }
+
+
+    }
+}
---
+++ ProjetoClassificados/ProjetoClassificados.Testes/Entidades/AnuncioTestes.cs
@@ -0,0 +1,28 @@
+﻿using ProjetoClassificados.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProjetoClassificados.Testes.Entidades
+{
+    public class AnuncioTestes
+    {
+        [Fact]
+        public void DeveRetornarSeOAnuncioForValido()
+        {
+            var anuncio = new Anuncio(
+                "ps4",
+                "ps4 usado",
+                "",
+                1000
+            );
+
+            Assert.True(anuncio.IsValid, "Anuncio é válido");
+        }
+
+
+    }
+}
